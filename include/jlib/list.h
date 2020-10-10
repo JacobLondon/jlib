45b41b7ee329a6f0dabc271105af24bd1371b25e
Jacob <jelondon12@gmail.com>
2020-10-10 11:22:55 -0700

    simpler interface

--- include/jlib/list.h
+++ include/jlib/list.h
@@ -7,70 +7,68 @@
 struct node {
 	void *value;
 	struct node *next;
-	struct node *prev;
-};
-
-struct list {
-	struct node *head;
-	struct node *tail;
-	size_t size;
-	void (*dtor)(void *buf);
 };
 
 /**
  * Create a new, empty list
+ * 
+ * dtor: Specity a way to free the value
  */
-struct list *list_new_dtor(void (*dtor)(void *buf));
-#define list_new() list_new_dtor(free)
+struct node *list_new();
 
 /**
  * Free every value in a list, every node, and the list itself
  */
-void list_free(struct list *self);
+void list_free(struct node *head);
 
 /**
  * Free a value and its node, rejoin adjacent nodes
  */
-void list_remove(struct list *self, struct node *cursor);
+void list_remove(struct node *head, struct node *self);
 
 /**
- * Make a new node at the head location
+ * Make a new node at the head location. Return the new head
  */
-struct node *list_push_front(struct list *self, void *value);
+struct node *list_push_front(struct node *head, void *value);
 
 /**
  * Make a new node at the tail location
  */
-struct node *list_push_back(struct list *self, void *value);
+void list_push_back(struct node *self, void *value);
 
 /**
  * Free the first node and its value, repoint the head node
  */
-void list_pop_front(struct list *self);
+void list_pop_front(struct node *head);
 
 /**
  * Free the last node and its value, repoint the tail node
  */
-void list_pop_back(struct list *self);
+void list_pop_back(struct node *self);
 
 /**
  * Make a new node after the current node pointer in the list
  */
-struct node *list_insert_next(struct list *self, struct node *cursor, void *value);
+void list_insert_next(struct node *self, void *value);
 
 /**
  * Make a new node before the current node pointer in the list
  */
-struct node *list_insert_prev(struct list *self, struct node *cursor, void *value);
+void list_insert_prev(struct node *self, void *value);
 
 /**
- * Free the next node and its value, rejoin the adjacent nodes
+ * First element
  */
-void list_remove_next(struct list *self, struct node *cursor);
+struct node *list_head(struct node *self);
 
 /**
- * Free the prev node and its value, rejoin the adjacent nodes
+ * Second element
  */
-void list_remove_prev(struct list *self, struct node *cursor);
+struct node *list_tail(struct node *self);
+
+/**
+ * Last element
+ */
+struct node *list_last(struct node *self);
 
 #endif /* JLIB_LIST_H */
--- src/list.c
+++ src/list.c
@@ -3,163 +3,116 @@
 #include <stdlib.h>
 #include <jlib/list.h>
 
-struct list *list_new_dtor(void (*dtor)(void *buf))
+struct node *list_new(void *value)
 {
-	struct list *self = calloc(1, sizeof(struct list));
-	assert(self);
-	self->head = self->tail;
-	self->dtor = dtor;
+	struct node *self = calloc(1, sizeof(struct node));
+	if (!self) {
+		return NULL;
+	}
+	self->value = value;
 	return self;
 }
 
-void list_free(struct list *self)
+void list_free(struct node *head)
 {
-	assert(self);
-
-	/* free each node */
-	while (self->head) {
-		list_remove(self, self->head);
-	}
-
-	memset(self, 0, sizeof(struct list));
-	free(self);
-}
-
-void list_remove(struct list *self, struct node *cursor)
-{
-	assert(self);
-
-	/* nothing to do */
-	if (!cursor) {
+	if (!head) {
 		return;
 	}
 
-	if (cursor->next) {
-		cursor->next->prev = cursor->prev;
-	}
-	// is tail
-	else {
-		self->tail = cursor->prev;
-	}
-
-	if (cursor->prev) {
-		cursor->prev->next = cursor->next;
-	}
-	// is head
-	else {
-		self->head = cursor->next;
-	}
-
-	if (cursor->value) {
-		free(cursor->value);
-	}
-
-	memset(cursor, 0, sizeof(struct node));
-	free(cursor);
+	list_free(head->next);
+	free(head);
 }
 
-struct node *list_push_front(struct list *self, void *value)
+void list_remove(struct node *head, struct node *self)
 {
-	return list_insert_prev(self, self->head, value);
-}
-
-struct node *list_push_back(struct list *self, void *value)
-{
-	return list_insert_next(self, self->tail, value);
-}
-
-void list_pop_front(struct list *self)
-{
-	list_remove(self, self->head);
-}
-
-void list_pop_back(struct list *self)
-{
-	list_remove(self, self->tail);
-}
-
-struct node *list_insert_next(struct list *self, struct node *cursor, void *value)
-{
-	assert(self);
-
-	struct node *new = malloc(sizeof(struct node));
-	assert(new);
-	new->value = value;
-
-	if (cursor) {
-		new->next = cursor->next;
-		new->prev = cursor;
-		cursor->next = new;
-
-		if (cursor->next) {
-			cursor->next->prev = new;
-		}
-		// is tail
-		else {
-			self->tail = new;
-		}
-	}
-	// inserting after at tail
-	else {
-		if (self->tail) {
-			new->prev = self->tail->prev;
-			new->prev->next = new;
-		}
-		else {
-			new->prev = NULL;
-		}
-		new->next = NULL;
-
-		self->tail = new;
+	if (!self) {
+		return;
 	}
 
-	return new;
-}
-
-struct node *list_insert_prev(struct list *self, struct node *cursor, void *value)
-{
-	assert(self);
-
-	struct node *new = malloc(sizeof(struct node));
-	assert(new);
-	new->value = value;
-
-	if (cursor) {
-		new->next = cursor;
-		new->prev = cursor->prev;
-		cursor->prev = new;
-
-		if (cursor->prev) {
-			cursor->prev->next = new;
-		}
-		// is head
-		else {
-			self->head = new;
-		}
-	}
-	// inserting before at head
-	else {
-		if (self->head) {
-			new->next = self->head->next;
-			new->next->prev = new;
-		}
-		else {
-			new->next = NULL;
-		}
-		new->prev = NULL;
-
-		self->head = new;
+	if (head == self) {
+		free(head);
+		return;
 	}
 
-	return new;
+	while (head) {
+		if (head->next == self) {
+			head->next = self->next;
+			free(self);
+			return;
+		}
+	}
 }
 
-void list_remove_next(struct list *self, struct node *cursor)
+struct node *list_push_front(struct node *head, void *value)
 {
-	list_remove(self, cursor->next);
+	struct node *next;
+
+	if (!head) {
+		return NULL;
+	}
+
+	next = list_new(value);
+	if (!next) {
+		return NULL;
+	}
+
+	next->next = head;
 }
 
-void list_remove_prev(struct list *self, struct node *cursor)
+void list_push_back(struct node *self, void *value)
 {
-	list_remove(self, cursor->prev);
+	struct node *next;
+
+	if (!self) {
+		return;
+	}
+
+	next = list_new(value);
+	if (!next) {
+		return;
+	}
+
+	while (self->next != NULL) {
+		self = self->next;
+	}
+
+	self->next = next;
+}
+
+void list_pop_front(struct node *head)
+{
+	if (!head) {
+		return;
+	}
+	// TODO
+}
+
+void list_pop_back(struct node *self)
+{
+
+}
+
+void list_insert_next(struct node *self, void *value)
+{
+
+}
+
+void list_insert_prev(struct node *self, void *value)
+{
+
+}
+
+struct node *list_head(struct node *self)
+{
+
+}
+
+struct node *list_tail(struct node *self)
+{
+
+}
+
+struct node *list_last(struct node *self)
+{
+
 }
